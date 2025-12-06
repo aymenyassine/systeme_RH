b57448f69ca44ede94d0baeddb8ce1d8ac45441d
CH4GU <aymenyassine801@gmail.com>
2025-12-06 18:29:41 +0100

    Initial commit - project setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Systeme_RH.Models;
+ 
+ public class ApplicationDbContext
+     : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Systeme_RH.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int? EmployeId{get; set;}
+
+    }
+}
